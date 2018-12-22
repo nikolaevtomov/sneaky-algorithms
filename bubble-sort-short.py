1ebb14eb575159eb764d36e114d3426a86cb537a
nikolaevtomov <nikolaevtomov@gmail.com>
2018-12-22 16:27:13 +0000

    bubble sort

---
+++ bubble-sort-short.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+
+import random
+import time
+from copy import copy
+
+
+def shortBubbleSort(aList):
+    # print('Initial list =', aList)
+    noOfSwaps = 0
+    noOfAssignments = 0
+    comparisons = 0
+    hasSwapped = True
+    passNum = len(aList) - 1
+    start = time.time()
+    while passNum > 0 and hasSwapped:
+        hasSwapped = False
+        for i in range(passNum):
+            comparisons = comparisons + 1
+            if aList[i] > aList[i + 1]:
+                temp = aList[i]
+                aList[i] = aList[i + 1]
+                aList[i + 1] = temp
+                noOfSwaps = noOfSwaps + 1
+                noOfAssignments = noOfAssignments + 3
+                hasSwapped = True
+        passNum = passNum - 1
+    elapsed = (time.time() - start)
+    # print('Sorted list = ', aList)
+    print('Comparisons =', comparisons)
+    print('Swaps =', noOfSwaps)
+    print('Assignments =', noOfAssignments)
+    print('Time taken =', elapsed, 'seconds')
+
+
+randomList = []
+for i in range(1, 4001):
+    randomList.append(random.randrange(1, 4001))
+
+
+print('shortBubbleSort()')
+shortBubbleSort(copy(randomList))
---
+++ bubble-sort.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+
+import random
+import time
+from copy import copy
+
+
+def bubbleSort(aList):
+    # print('Initial list =', aList)
+    noOfSwaps = 0
+    noOfAssignments = 0
+    comparisons = 0
+    start = time.time()
+    for passNum in range(len(aList) - 1, 0, -1):
+        for i in range(passNum):
+            comparisons = comparisons + 1
+            if aList[i] > aList[i + 1]:
+                temp = aList[i]
+                aList[i] = aList[i + 1]
+                aList[i + 1] = temp
+                noOfSwaps = noOfSwaps + 1
+                noOfAssignments = noOfAssignments + 3
+    elapsed = (time.time() - start)
+    # print('Sorted list = ', aList)
+    print('Comparisons =', comparisons)
+    print('Swaps =', noOfSwaps)
+    print('Assignments =', noOfAssignments)
+    print('Time taken =', elapsed, 'seconds')
+
+
+randomList = []
+for i in range(1, 4001):
+    randomList.append(random.randrange(1, 4001))
+
+
+print('bubbleSort()')
+bubbleSort(copy(randomList))
